9f6e52e3abbd9fb7b56c409d04dc4c23faed6c00
Sakshi Gupta <guptasakshi1914@gmail.com>
2026-04-22 22:20:31 +0530

    Time: 44 ms (61.12%), Space: 113.2 MB (58.56%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -0,0 +1,37 @@
+class RandomizedSet {
+public:
+    vector<int> nums;
+    unordered_map<int, int> mp;
+
+    RandomizedSet() {
+        
+    }
+    
+    bool insert(int val) {
+        if (mp.count(val)) return false;
+        
+        nums.push_back(val);
+        mp[val] = nums.size() - 1;
+        return true;
+    }
+    
+    bool remove(int val) {
+        if (!mp.count(val)) return false;
+        
+        int idx = mp[val];
+        int last = nums.back();
+        
+        // swap with last element
+        nums[idx] = last;
+        mp[last] = idx;
+        
+        nums.pop_back();
+        mp.erase(val);
+        
+        return true;
+    }
+    
+    int getRandom() {
+        return nums[rand() % nums.size()];
+    }
+};
